a054ffb87add056ad7e4aff9e784ba429e2db0aa
Alina Borozniak <borozniakalina@gmail.com>
2026-03-02 14:59:14 +0200

    Create input and output empty functions

---
+++ app/io/input.py
@@ -0,0 +1,6 @@
+def input_from_concole():
+    pass
+def read_file_python():
+    pass
+def read_file_pandas():
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,4 @@
+def write_into_console():
+    pass
+def write_into_file_python():
+    pass
